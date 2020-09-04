d5e160bbd1293ac9dfc3b66db044d6d3ed3d11f3
ampsgger <bobobao123@gmail>
2020-09-04 15:26:46 +0800

    lab_04

--- HelloWorld123.java
+++ HelloWorld123.java
@@ -1,7 +1,9 @@
 package xxx;
 	public class HelloWorld{ //類別宣告
 		public static void main(String[] args) { /*main方法宣告，其包含兩個
-												修飾子“public"、”static，“void"為回傳值型態。*/		
+												修飾子“public"、”static，“void"為回傳值型態。*/
+												
+			int aa = 1;											
 			System.out.println("Hello World!!!");//TEST_GIT_2nd
 			//GIT_3rd
 			//000000000000_branch_new123
