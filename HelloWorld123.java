e05124689376f708c12aaa1f2dcccb0203db37ad
ampsgger <bobobao123@gmail>
2020-09-04 14:58:46 +0800

    3rd

--- HelloWorld123.java
+++ HelloWorld123.java
@@ -3,6 +3,7 @@ package xxx;
 		public static void main(String[] args) { /*main方法宣告，其包含兩個
 												修飾子“public"、”static，“void"為回傳值型態。*/		
 			System.out.println("Hello World!!!");//TEST_GIT_2nd
+			//GIT_3rd
 			
 		}
 	}
